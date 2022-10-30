6880db3a53027332baaa78f24f155a76074d2c09
Luis Hdz <luishernandez0626@outlook.com>
2022-10-30 11:20:40 -0500

    Practica 2 terminada

--- Unidad2/Practica2/mainapp/pelicula/admin.py
+++ Unidad2/Practica2/mainapp/pelicula/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Pelicula
+from .models import Pelicula, Clasificacion
 
 # Register your models here.
 
 admin.site.register(Pelicula)
+admin.site.register(Clasificacion)
